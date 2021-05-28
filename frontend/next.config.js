cd85d9f4b5865d503a200129052b7fd4c92518fc
narimanadam <narimanadam7@gmail.com>
2021-05-29 02:41:20 +0400

    update nextjs config file

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -10,9 +10,11 @@ module.exports = {
     IMAGES_DOMAIN: process.env.IMAGES_DOMAIN,
   },
   images: {
-    deviceSizes: [640, 768, 1024, 1280, 1600],
-    imageSizes: [16, 32, 48, 64, 96],
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
     domains: [process.env.IMAGES_DOMAIN],
+    path: "/_next/image",
+    loader: "default",
   },
   ...nextTranslate(),
 };
